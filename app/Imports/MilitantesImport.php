2454dd502e0daf726d28f2221e3e787cc5d3ba3b
root <root@22LAP5CD1195RCT.indra.es>
2022-11-15 12:46:40 -0500

    conele 1.0

--- app/Imports/MilitantesImport.php
+++ app/Imports/MilitantesImport.php
@@ -31,7 +31,7 @@ use Maatwebsite\Excel\Events\BeforeImport;
 use Maatwebsite\Excel\Events\ImportFailed;
 use function PHPUnit\Framework\isNull;
 
-class MilitantesImport implements OnEachRow, SkipsOnFailure, SkipsOnError, SkipsEmptyRows, ShouldQueue, WithEvents, WithHeadingRow, WithValidation, WithChunkReading
+class MilitantesImport implements OnEachRow, SkipsOnFailure, SkipsOnError, ShouldQueue, SkipsEmptyRows, WithEvents, WithHeadingRow, WithValidation, WithChunkReading
 {
     public $id, $importedBy;
 
@@ -45,9 +45,9 @@ class MilitantesImport implements OnEachRow, SkipsOnFailure, SkipsOnError, Skips
     public function registerEvents(): array
     {
         return [
-            ImportFailed::class => function(ImportFailed $event) {
-                $this->importedBy->notify(new ImportHasFailedNotification);
-            },
+            //ImportFailed::class => function(ImportFailed $event) {
+            //    $this->importedBy->notify(new ImportHasFailedNotification);
+            //},
 
             BeforeImport::class => function (BeforeImport $event) {
                 $totalRows = $event->getReader()->getTotalRows();
@@ -76,8 +76,8 @@ class MilitantesImport implements OnEachRow, SkipsOnFailure, SkipsOnError, Skips
         cache()->forever("current_row_{$this->id}", $rowIndex);
 
         $militante = null;
-        if ($row['cedula'] != '' && $row['cedula'] != null) {
-            $militante = Militante::where('documento', $row['cedula'])
+        if ($row['documento'] != '' && $row['documento'] != null) {
+            $militante = Militante::where('documento', $row['documento'])
                                    ->first();
         } elseif ($row['correo'] != '' && $row['correo'] != null) {
             $militante = Militante::where('email', $row['correo'])
@@ -94,8 +94,10 @@ class MilitantesImport implements OnEachRow, SkipsOnFailure, SkipsOnError, Skips
                 $militante->fechaingreso = $row['fecha_de_ingreso']?Carbon::createFromFormat('m/d/Y', $row['fecha_de_ingreso'])->format('Y-m-d'):null;
                 $militante->idinscripcion = $row['id_inscripcion'];
                 $militante->iddepartamento = $row['id_departamento']?$row['id_departamento']:null;
+                $militante->idtipos_documento = $row['idtipos_documento']?$row['idtipos_documento']:null;
+
                 $militante->idciudad = $row['id_ciudad']?$row['id_ciudad']:null;
-                $militante->documento = $row['cedula'];
+                $militante->documento = $row['documento'];
                 $militante->fechanacimiento = $row['fecha_de_nacimiento']?Carbon::createFromFormat('m/d/Y', $row['fecha_de_nacimiento'])->format('Y-m-d'):null;
                 $militante->movil = $row['celular'];
                 $militante->email = $row['correo'];
@@ -114,8 +116,7 @@ class MilitantesImport implements OnEachRow, SkipsOnFailure, SkipsOnError, Skips
                 $militante->electo  = $row['electo']?$row['electo']:null;
                 $militante->votos  = $row['votos']?$row['votos']:null;
 
-                $militante->username = $row['cedula'];
-                $militante->idtipos_documento = 1;
+                $militante->username = $row['documento'];
                 $militante->idpais = 1;
                 $militante->observaciones = 'Importado'.$row['observaciones']?"\n".$row['observaciones']:'';
 
@@ -125,6 +126,48 @@ class MilitantesImport implements OnEachRow, SkipsOnFailure, SkipsOnError, Skips
                 $militante->save();
                 $rol = Rol::where('id', 3)->first();
                 $militante->assignRole($rol->nombre);
+                MilitanteController::setHistorial($militante->id, 1, $militante->observaciones, $this->importedBy->id);
+                DB::commit();
+
+            } catch (Throwable $e){
+                DB::rollBack();
+            }
+        } else {
+            try{
+                DB::beginTransaction();
+                $militante->fechaingreso = $row['fecha_de_ingreso']?Carbon::createFromFormat('m/d/Y', $row['fecha_de_ingreso'])->format('Y-m-d'):null;
+                $militante->idinscripcion = $row['id_inscripcion']?$row['id_inscripcion']:$militante->idinscripcion;
+                $militante->iddepartamento = $row['id_departamento']?$row['id_departamento']:$militante->iddepartamento;
+                $militante->idtipos_documento = $row['idtipos_documento']?$row['idtipos_documento']:$militante->idtipos_documento;
+
+                $militante->idciudad = $row['id_ciudad']?$row['id_ciudad']:$militante->idciudad;
+                $militante->documento = $row['documento']?$row['documento']:$militante->documento;
+                $militante->fechanacimiento = $row['fecha_de_nacimiento']?Carbon::createFromFormat('m/d/Y', $row['fecha_de_nacimiento'])->format('Y-m-d'):$militante->fechanacimiento;
+                $militante->movil = $row['celular']?$row['celular']:$militante->movil;
+                $militante->email = $row['correo']?$row['correo']:$militante->email;
+                $militante->direccion = $row['direccion']?$row['direccion']:$militante->direccion;
+                $militante->nombre = $row['nombre']?$row['nombre']:$militante->nombre;
+                $militante->apellido = $row['apellido']?$row['apellido']:$militante->apellido;
+
+                $militante->avalado = $row['avalado']?$row['avalado']:$militante->avalado;
+                $militante->idcorporacion = $row['idcorporacion']?$row['idcorporacion']:$militante->idcorporacion;
+                $militante->idgenero  = $row['idgenero']?$row['idgenero']:$militante->idgenero;
+                $militante->renuncio = $row['renuncio']?$row['renuncio']:$militante->renuncio;
+                $militante->fecharenuncia = $row['fecharenuncia']?Carbon::createFromFormat('m/d/Y', $row['fecharenuncia'])->format('Y-m-d'):$militante->fecharenuncia;
+                $militante->coalicion = $row['coalicion']?$row['coalicion']:$militante->coalicion;
+                $militante->nombrecoalicion = $row['nombrecoalicion']?$row['nombrecoalicion']:$militante->nombrecoalicion;
+                $militante->idgrupoetnico  = $row['idgrupoetnico']?$row['idgrupoetnico']:$militante->idgrupoetnico;
+                $militante->electo  = $row['electo']?$row['electo']:$militante->electo;
+                $militante->votos  = $row['votos']?$row['votos']:$militante->votos;
+
+                $militante->username = $row['documento']?$militante->username:$militante->username;
+                $militante->idpais = 1;
+                $militante->observaciones = 'Importado Modificado'.$row['observaciones']?"\n".$row['observaciones']:'';
+
+                $militante->password = Hash::make($militante->documento);
+                $militante->estado = $militante->avalado?1:3;
+                $militante->changedpassword = null;
+                $militante->save();
                 MilitanteController::setHistorial($militante->id, 1, $militante->observaciones, $this->importedBy->id);
                 DB::commit();
 
@@ -145,10 +188,10 @@ class MilitantesImport implements OnEachRow, SkipsOnFailure, SkipsOnError, Skips
             'id_ciudad' => 'nullable|numeric',
             //'cedula' => 'nullable|string|numeric',
             //'fecha_de_nacimiento' => 'nullable|date_format:m/d/Y', //'nullable|regex:/([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})/',
-            'nombre_completo' => 'nullable|string',
+            //'nombre_completo' => 'nullable|string',
             //'celular' => 'nullable|numeric',
             //'correo_electronico' => 'nullable|string',
-            'direccion' => 'nullable|string'
+            //'direccion' => 'nullable|string'
         ];
     }
 
